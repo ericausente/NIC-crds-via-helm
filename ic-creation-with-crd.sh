b81025eb7ef59b671d9db40a859373b0ec861c23
ericausente <eric_ausente@yahoo.com>
2023-02-05 13:18:12 +0800

    Update ic-creation-with-crd.sh

--- ic-creation-with-crd.sh
+++ ic-creation-with-crd.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#helm install mbs-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set prometheus.create=true
+#helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set prometheus.create=true
 
 ## With External Traffic Policy set to Cluster (since by default it is set to Local)
 
-#helm install mbs-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.service.externalTrafficPolicy=Cluster --set prometheus.create=true
+#helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.service.externalTrafficPolicy=Cluster --set prometheus.create=true
 
 #removal of cluster as externaltrafficpolicy type and adding the replica as 2 and daemonset kind
 
-helm install mbs-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.replicaCount=1 --set controller.kind=daemonset --set prometheus.create=true --set controller.readyStatus.initialDelaySeconds=30
+helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.replicaCount=1 --set controller.kind=daemonset --set prometheus.create=true --set controller.readyStatus.initialDelaySeconds=30
 
 echo
 echo
