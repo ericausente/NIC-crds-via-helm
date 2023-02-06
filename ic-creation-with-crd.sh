dea1f7b20749b2c7b9ba97a0d722f327d1dc7c6f
ericausente <eric_ausente@yahoo.com>
2023-02-06 10:35:51 +0800

    Update ic-creation-with-crd.sh

--- ic-creation-with-crd.sh
+++ ic-creation-with-crd.sh
@@ -4,12 +4,13 @@
 
 ## With External Traffic Policy set to Cluster (since by default it is set to Local)
 
-#helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.service.externalTrafficPolicy=Cluster --set prometheus.create=true
+#helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=3.0.1 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.service.externalTrafficPolicy=Cluster --set prometheus.create=true
 
 #removal of cluster as externaltrafficpolicy type and adding the replica as 2 and daemonset kind
 
 helm uninstall my-ingress
-helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=2.3.0 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.replicaCount=1 --set controller.kind=daemonset --set prometheus.create=true --set controller.readyStatus.initialDelaySeconds=30
+
+helm install my-ingress nginx-stable/nginx-ingress --set controller.image.repository=private-registry.nginx.com/nginx-ic-nap/nginx-plus-ingress --set controller.nginxplus=true --set controller.appprotect.enable=true --set controller.image.tag=3.0.1 --set controller.serviceAccount.imagePullSecretName=regcred --set controller.service.type=NodePort --set controller.service.httpsPort.nodePort=32137 --set controller.replicaCount=1 --set controller.kind=deployment --set prometheus.create=true --set controller.readyStatus.initialDelaySeconds=30 --set controller.service.externalTrafficPolicy=Cluster
 
 echo
 echo
@@ -26,8 +27,6 @@ echo What ready-made example would you like to deploy? Paste the exact name:
 read path
 echo You chose: $path
 echo Now applying all the yaml resources under ../kubernetes-ingress/examples/custom-resources/$path
-#echo Your second car was: $car2
-#echo Your third car was: $car3
 
 for i in ../kubernetes-ingress/examples/custom-resources/$path/*.yaml
 do kubectl apply -f $i
